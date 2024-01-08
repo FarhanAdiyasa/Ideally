fee60e1b26a773b49f59c68e8457c79b3c149774
ivienrose <shadrinaarinf@gmail.com>
2024-01-08 09:37:02 +0700

    tambahan kategori tiap brand

--- app/Http/Controllers/AgrigardController.php
+++ app/Http/Controllers/AgrigardController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Storage;
 
 class AgrigardController extends Controller
 {
-        public function indexBrand()
+    public function indexBrand()
     {
         $agrigards = Agrigard::where('stok', '>', 0)->inRandomOrder()->take(12)->get();
         $agrigardbaru = Agrigard::where('stok', '>', 0)->latest('created_at')->inRandomOrder()->take(4)->get();
@@ -33,9 +33,19 @@ class AgrigardController extends Controller
         $agrigards = Agrigard::findOrFail($id_agrigard);
         $kategori = $agrigards->kategori;
         $agrigardterkait = Agrigard::where('stok', '>', 0)->where('kategori', '=', $agrigards->kategori)
-        ->where('id_agrigard', '!=', $agrigards->id_agrigard)->inRandomOrder()->take(8)->get();
+            ->where('id_agrigard', '!=', $agrigards->id_agrigard)->inRandomOrder()->take(8)->get();
         $agrigardbaru = Agrigard::where('stok', '>', 0)->latest('created_at')->inRandomOrder()->take(8)->get();
         $agrigardpilihan = Agrigard::where('stok', '>', 0)->inRandomOrder()->take(8)->get();
         return view('agrigards.details', compact('agrigards', 'agrigardterkait', 'agrigardbaru', 'agrigardpilihan'));
+    }
+
+    public function showcase2(Request $request)
+    {
+        $agrigards = Agrigard::where('stok', '>', 0)->when($request->filled('kategori'), function ($query) use ($request) {
+            return $query->where('kategori', $request->input('kategori'));
+        })
+            ->paginate(20);
+
+        return view('agrigards.showcase', compact('agrigards'));
     }
 }
--- resources/views/Agrigards/showcase.blade.php
+++ resources/views/Agrigards/showcase.blade.php
@@ -61,58 +61,52 @@
         <div class="container mt-4">
             <div class="inner-container top-end bg-orange">
                 <div class="row">
-                    <div class="col wrapper">
+                <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Pot Plastik</p>
+                            <a href="{{ route('agrigard.kategori', ['kategori' => 'Pot Plastik']) }}">Pot Plastik</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Pot Plastik</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Pot Terracota']) }}">Pot Terracota</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Pot Terracota</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Bibit Tanaman']) }}">Bibit Tanaman</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Bibit Tanaman</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Media Tanam']) }}">Media Tanam</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Media Tanam</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Wadah Tanam']) }}">Wadah Tanam</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Wadah Tanam</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Obat dan Nutrisi']) }}">Obat dan Nutrisi</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Obat dan Nutrisi</p>
+                        <a href="{{ route('agrigard.kategori', ['kategori' => 'Alat Kerja']) }}">Alat Kerja</a>
                         </div>
                     </div>
                     <div class="col wrapper">
                         <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
                         <div class="text-white text-center m-0">
-                            <p>Alat Kerja</p>
-                        </div>
-                    </div>
-                    <div class="col wrapper">
-                        <div class="bg-light rounded-circle" style="height: 25px; width: 25px;"></div>
-                        <div class="text-white text-center m-0">
-                            <p>Lain - lain</p>
+                            <a href="{{ route('agrigard.kategori', ['kategori' => 'Lain - lain']) }}">Lain - lain</a>
                         </div>
                     </div>
                 </div>
